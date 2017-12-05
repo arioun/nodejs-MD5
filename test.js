c1f4f0c6381d5f36e312eda725b8345a068f0555
Your Name <you@example.com>
2017-12-05 15:12:29 +0800

    12/5 commit

--- test.js
+++ test.js
@@ -3,27 +3,27 @@ var assert = require('assert');
 
 describe('md5', function () {
 
-  it('should throw an error for an undefined value', function() {
+  it('未定义的值应该报错', function() {
     assert.throws(function() {
       md5(undefined);
     });
   });
 
-  it('should allow the hashing of the string `undefined`', function() {
+  it('应该允许未定义的哈希字符串', function() {
     assert.equal('5e543256c480ac577d30f76f9120eb74', md5('undefined'));
   });
 
-  it('should throw an error for `null`', function() {
+  it('应该报错： `null`', function() {
     assert.throws(function() {
       md5(null);
     });
   });
 
-  it('should return the expected MD5 hash for "message"', function() {
+  it('应该返回"message"的正确MD5值', function() {
     assert.equal('78e731027d8fd50ed642340b7c9a63b3', md5('message'));
   });
 
-  it('should not return the same hash for random numbers twice', function() {
+  it('不应该返回同样的哈希值随机数两次', function() {
     var msg1 = Math.floor((Math.random() * 100000) + 1) + (new Date).getTime();
     var msg2 = Math.floor((Math.random() * 100000) + 1) + (new Date).getTime();
 
@@ -34,13 +34,13 @@ describe('md5', function () {
     }
   });
 
-  it('should support Node.js Buffers', function() {
+  it('应该支持Node.js的Buffers', function() {
     var buffer = new Buffer('message áßäöü', 'utf8');
 
     assert.equal(md5(buffer), md5('message áßäöü'));
   })
 
-  it('should be able to use a binary encoded string', function() {
+  it('应该可以使用一个二进制编码字符串', function() {
     var hash1 = md5('abc', { asString: true });
     var hash2 = md5(hash1 + 'a', { asString: true, encoding : 'binary' });
     var hash3 = md5(hash1 + 'a', { encoding : 'binary' });
